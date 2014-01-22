e90bc99b47d1cf63a85b3552910c19db71935ca4
Pinhas Yakubov <pinhas.yak@gmail.com>
2014-01-22 10:33:56 +0200

    add Default controller

---
+++ web/src/main/java/com/pi/javateam/web/DefaultController.java
@@ -0,0 +1,12 @@
+package com.pi.javateam.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DefaultController {
+    @RequestMapping(value = "/")
+    public String myDefaultPage(){
+        return "EvenytDetails";
+    }
+}
