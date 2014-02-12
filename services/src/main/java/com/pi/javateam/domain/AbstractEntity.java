51d8cfa005ebb5c5f9879dbe48417c9450639c10
Pinhas Yakubov <pinhas.yak@gmail.com>
2014-02-12 16:04:59 +0200

    Add Team domain

---
+++ services/src/main/java/com/pi/javateam/domain/AbstractEntity.java
@@ -0,0 +1,46 @@
+package com.pi.javateam.domain;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+import org.springframework.hateoas.Identifiable;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+/**
+ * Created by pi on 2/12/14.
+ */
+@MappedSuperclass
+public class AbstractEntity implements Identifiable<Long> {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonIgnore
+    private final Long id;
+
+    protected AbstractEntity() {
+        this.id = null;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AbstractEntity)) return false;
+
+        AbstractEntity that = (AbstractEntity) o;
+
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
